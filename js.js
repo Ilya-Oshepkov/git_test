9e8427439ff6b13cca19bf630efe3e334c1c1246
Ilya-Oshepkov <ilya-oshepkov@mail.ru>
2017-10-20 01:18:14 +0500

    case в js

--- js.js
+++ js.js
@@ -1,2 +1,25 @@
 var git ='Hello git';
-alert(git);
+
+var nam = prompt("введите число ","");
+switch (nam){
+    case 'string':
+        alert("введите число");
+        break;
+    default:
+        alert ('это'+ nam);
+        break;
+}
+
+/*var isAdmin = confirm("Вы - администратор?");
+alert (isAdmin);
+switch (isAdmin){
+    case 'true':      
+    alert ( 'good' );
+        break;
+
+    case 'false' :   
+    alert ('bad');
+        break;
+}
+
+
